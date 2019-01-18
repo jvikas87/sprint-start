2f87d9d2229dae79d64b109fae203cdbc2809ebf
Vikas <=>
2019-01-18 08:51:15 +0530

    Initial Commit

---
+++ src/main/java/com/example/practise/PractiseApplication.java
@@ -0,0 +1,28 @@
+package com.example.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class PractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractiseApplication.class, args);
+	}
+
+}
+@Controller
+class MyController{
+
+	@RequestMapping(value = "/index",method = RequestMethod.GET)
+	@ResponseBody
+	public String sayHello(){
+		return "Hello World";
+	}
+
+}
+
